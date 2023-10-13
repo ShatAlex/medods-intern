70535ea79c890011e7772ce381e394c3f1a24ebc
ShatAlex <Shaturnyyy2@gmail.com>
2023-10-13 13:42:46 +0300

    rework thirdPartyAPI

--- app/controllers/mock_data_controller.rb
+++ app/controllers/mock_data_controller.rb
@@ -7,26 +7,42 @@ require 'json'
 
 class MockDataController < ApplicationController
 
-    def get_mock_data
-        request_text = Faker::Quote.famous_last_words 
-        render json: {data: request_text}, status: 200
+    def get_unii
+
+        uri = URI("https://api.fda.gov/other/unii.json?limit=1000")
+
+        Net::HTTP.start(uri.host, uri.port, :use_ssl => true)  do |http|
+            request = Net::HTTP::Get.new uri
+    
+            response = http.request request
+            
+            response_json = JSON.parse(response.read_body)
+    
+            data = response_json['results'][Random.rand(999)]['unii']
+
+            render json: {data: data}, status: 200
+            
+        end
     end
 
     def get_analysis
 
-        uri = URI("https://api.adviceslip.com/advice")
+        url = "https://api.fda.gov/other/unii.json?search=unii:"+params[:data]
+
+        uri = URI(url)
 
         Net::HTTP.start(uri.host, uri.port, :use_ssl => true)  do |http|
-          request = Net::HTTP::Get.new uri
-  
-          response = http.request request
-          
-          response_json = JSON.parse(response.read_body)
-  
-          analysis = response_json["slip"]["advice"]
+            request = Net::HTTP::Get.new uri
+    
+            response = http.request request
+            
+            response_json = JSON.parse(response.read_body)
 
-          render json: {analysis: analysis}, status: 200
-        
+            analysis = response_json["results"][0]['substance_name']
+
+            render json: {analysis: analysis}, status: 200
+            
         end
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   post '/consultation_requests/:request_id/recommendations', to: 'recommendations#create'
   get '/patients/:patient_id/recommendations', to: 'recommendations#get_all_by_patient_id'
   
-  get '/mock_data', to: 'mock_data#get_mock_data'
+  get '/mock_data', to: 'mock_data#get_unii'
   get '/mock_data/analysis', to: 'mock_data#get_analysis'
 end
